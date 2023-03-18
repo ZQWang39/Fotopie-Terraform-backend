dc64fbb4a30ccce0358b5d097b15e9c3bd0a9e38
Ziqi Wang <ziqi.wang39@gmail.com>
2023-03-18 17:42:37 +1000

    Add AWS-grafana for monitoring

--- aws_grafana.tf
+++ aws_grafana.tf
@@ -14,4 +14,14 @@ module "managed-service-grafana" {
   }
   data_sources              = ["AMAZON_OPENSEARCH_SERVICE", "ATHENA", "CLOUDWATCH", "REDSHIFT", "SITEWISE", "TIMESTREAM", "PROMETHEUS", "XRAY"]
   notification_destinations = ["SNS"]
+
+  role_associations = {
+    "ADMIN" = {
+      "group_ids" = ["24685468-f0e1-70ef-04ef-d4c7172689b9"]
+    }
+    "ADMIN" = {
+      "user_ids" = ["a4d894f8-60f1-7020-0df7-8c417d67d35d"]
+    }
+   
+  }
 }
