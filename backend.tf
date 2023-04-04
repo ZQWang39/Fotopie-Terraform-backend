360d06f2be419622b513c9ec83dc4502df4ff64e
Ziqi Wang <ziqi.wang39@gmail.com>
2023-04-04 11:52:02 +1000

    Add S3 backend back

--- backend.tf
+++ backend.tf
@@ -1,12 +1,12 @@
-# terraform {
-#   required_version = ">= 0.12"
-#   required_providers {
-#      aws = {
-#       source = "hashicorp/aws"
-#       version = "4.59.0"
-#     }
-#   }
-#   backend "s3" {
+terraform {
+  required_version = ">= 0.12"
+  required_providers {
+     aws = {
+      source = "hashicorp/aws"
+      version = "4.59.0"
+    }
+  }
+  backend "s3" {
 
-#     }
-# }
+    }
+}
